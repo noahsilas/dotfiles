3c94bc2fa29e298969791945e07996135d18501b
Noah Silas <noahking@gmail.com>
2023-11-30 00:58:02 +0000

    Exit kf completer fast on unknown arg
    If I hit `tab` on a second argument, I want a nice quick notification
    that I'm making a mistake. By moving the kafka-consumer-groups.sh
    invocation into a stateful block, we only execute it when completing the
    argument in position 1, and exit fast otherwise.

--- dotfiles/zshrc.d/fpath/_kf-check-consumer-group.sh
+++ dotfiles/zshrc.d/fpath/_kf-check-consumer-group.sh
@@ -1,6 +1,10 @@
 #compdef kf-check-consumer-group.sh
 
-local result=$(kafka-consumer-groups.sh --bootstrap-server kafka.hstl.io:9092 --list 2>/dev/null)
-local -a consumerGroups=( ${(f)result} )
+_arguments '1:consumer group name:->GroupName'
 
-_arguments '1:consumer group name:(${consumerGroups})'
+case "$state" in
+  GroupName)
+    local result=$(kafka-consumer-groups.sh --bootstrap-server kafka.hstl.io:9092 --list 2>/dev/null)
+    _values "Consumer group name" ${(f)result}
+    ;;
+esac
